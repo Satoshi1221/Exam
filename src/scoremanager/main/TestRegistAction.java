fdf6e9c581c6f83d964c5a333f72cd8bdaf92006
oohara <oohara@172.16.31.84>
2024-04-26 14:42:47 +0900

    4/26 山岸九十九

--- src/scoremanager/main/TestRegistAction.java
+++ src/scoremanager/main/TestRegistAction.java
@@ -4,13 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-<<<<<<< HEAD
-import bean.Teacher;
-import dao.ClassNumDao;
-=======
 import bean.Subject;
 import bean.Test;
->>>>>>> branch 'master' of https://github.com/Satoshi1221/Exam.git
 import tool.Action;
 
 public class TestRegistAction extends Action {
@@ -18,36 +13,15 @@ public class TestRegistAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		HttpSession session = req.getSession();
-<<<<<<< HEAD
-		Teacher teacher = (Teacher)session.getAttribute("user");s
-		String classNum = "";
-		ClassNumDao cDao = new ClassNumDao();
-		classList = cDao.filter(teacher.getSchool());
-		String entYearStr = "";
-
-=======
 
 		Class_Num class_num = new Class_Num();
 		Subject subject = new Subject();
 		Test test = new Test();
->>>>>>> branch 'master' of https://github.com/Satoshi1221/Exam.git
 
 		req.getRequestDispatcher("test_regist.jsp").forward(req, res);
 	}
-<<<<<<< HEAD
-
-	private void setRequestData(HttpServletRequest req, HttpServletResponse res) throws Exception {
-
-
-		req.getRequestDispatcher("test_regist.jsp").forward(req, res);
-=======
 	private void setRequest(HttpServletRequest req, HttpServletResponse res) throws Exception{
-<<<<<<< HEAD
 
 
-=======
-		return ;
->>>>>>> branch 'master' of https://github.com/Satoshi1221/Exam.git
->>>>>>> branch 'master' of https://github.com/Satoshi1221/Exam.git
 	}
 }
