bd1d6fecec10e548f2eb5e0e7251e169994f58c2
oohara <oohara@172.16.31.104>
2024-05-09 14:34:46 +0900

    5/9 山岸九十九

--- src/scoremanager/main/TestListSubjectExecuteAction.java
+++ src/scoremanager/main/TestListSubjectExecuteAction.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.Teacher;
+import bean.Test;
 import tool.Action;
 
 public class TestListSubjectExecuteAction extends Action {
@@ -15,23 +16,22 @@ public class TestListSubjectExecuteAction extends Action {
 		HttpSession session = req.getSession();  // セッション
 		Teacher teacher = (Teacher)session.getAttribute("user");
 
-
 		//入力された値をいれる
 		String entYear = ""; //入学年度
 		String classNum = ""; //クラス
 		String score = ""; //科目成績データ
 
 		if (score == null) {
-			test.setentYear(entYear);
-			test.setClassNum(classNum);
-			test.setscore(score);
+			Test.setEntYear(entYear);
+			Test.setClassNum(classNum);
+			Test.setPoint(point);
 			req.getRequestDispatcher("test_list.jsp").forward(req, res);
 		} else {
-			test.setentYear(entYear);
-			test.setClassNum(classNum);
-			test.setscore(score);
-			boolean save = tDao.save(test);
-
+			Test.setEntYear(entYear);
+			Test.setClassNum(classNum);
+			Test.setPoint(point);
+			boolean save = tDao.save(test)
+		}
 
 
 		//リクエストパラメータ取得
