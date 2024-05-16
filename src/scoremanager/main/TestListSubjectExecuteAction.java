904658427ccb3f07cde7aadf0db58b8a9edd87f6
oohara <oohara@DESKTOP-6H4GQEO>
2024-05-16 11:31:03 +0900

    5月16日 川端

--- src/scoremanager/main/TestListSubjectExecuteAction.java
+++ src/scoremanager/main/TestListSubjectExecuteAction.java
@@ -6,10 +6,7 @@ import javax.servlet.http.HttpSession;
 
 import bean.Teacher;
 import bean.Test;
-<<<<<<< HEAD
 import dao.TestDao;
-=======
->>>>>>> branch 'master' of https://github.com/Satoshi1221/Exam.git
 import tool.Action;
 
 public class TestListSubjectExecuteAction extends Action {
@@ -21,7 +18,7 @@ public class TestListSubjectExecuteAction extends Action {
 		Teacher teacher = (Teacher)session.getAttribute("user");
 		Test test = new Test();
 
-<<<<<<< HEAD
+
 		// 入力された値をいれる
 		String entYear = req.getParameter("f1"); //入学年度
 		String classNum = req.getParameter("f2"); //クラス
@@ -30,44 +27,11 @@ public class TestListSubjectExecuteAction extends Action {
 
 		// 項目が未入力の場合
 		if (entYear.equals("0") || classNum.equals("0") || subject.equals("0")) {
-=======
-		//入力された値をいれる
-		String entYear = ""; //入学年度
-		String classNum = ""; //クラス
-		String score = ""; //科目成績データ
-
-		//チェック
-		if (entYear == null) {
-			Test.setEntYear(entYear);
-			Test.setclassNum(classNum);
-			Test.setPoint(point);
->>>>>>> branch 'master' of https://github.com/Satoshi1221/Exam.git
-			req.getRequestDispatcher("test_list.jsp").forward(req, res);
-		} else {
-<<<<<<< HEAD
-			test.setEntYear(entYear);
-			test.setClassNum(classNum);
-			test.setscore(subject);
-=======
-			Test.setEntYear(entYear);
-			Test.setclassNum(classNum);
-			Test.setPoint(point);
->>>>>>> branch 'master' of https://github.com/Satoshi1221/Exam.git
-			boolean save = tDao.save(test);
+			req.getParameter("test_list.jsp");
 		}
 
-		//リクエストパラメータ取得
-		entYear = req.getParameter("1"); //入学年度
-		classNum = req.getParameter("2"); //クラス
-		score = req.getParameter("3"); //科目成績データ
-
-		//レスポンス値入れる
-		req.setAttribute("1",entYear);
-		req.setAttribute("2",classNum);
-		req.setAttribute("3",score);
-
-		//フォワード{
-			req.getRequestDispatcher("test_list_student.jsp").forward(req, res);{
+		// フォワード{
+		req.getRequestDispatcher("test_list_student.jsp").forward(req, res);{
 
 		}
 
